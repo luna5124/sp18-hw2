f12c452139aafa2c8ae2c4ef8ebd314b4c062026
Lihui Lu <lunarlu5124@gmail.com>
2018-05-08 18:49:14 -0700

    convert millisecond to second

--- src/main/java/globesort/GlobeSortClient.java
+++ src/main/java/globesort/GlobeSortClient.java
@@ -55,8 +55,8 @@ public class GlobeSortClient {
         System.out.println("Total time used: " + diff);
         long sort_time = response.getTime();
         System.out.println("Sorting time: " + sort_time);
-        System.out.println("Application throughput: " + values.length/sort_time);
-        System.out.println("One way: " + values.length/((diff - sort_time)/2));
+        System.out.println("Application throughput: " + values.length/(sort_time/1000);
+        System.out.println("One way: " + values.length/((diff - sort_time)/2000));
         System.out.println("Sorted array");
     }
 
